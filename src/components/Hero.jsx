287000f028e78775e39700d9865050ceaf24957a
Aakanksha <82.25aakanksha@gmail.com>
2024-04-20 15:59:42 +0530

    added social linkes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,84 @@
-import React from 'react'
+import React from "react";
 
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 const Hero = () => {
   return (
-    <section className='relative w-full mx-auto h-screen '>
-     <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-     <div className='flex flex-col justify-center items-center mt-5'>
-     <div className='w-5 h-5 rounded-full bg-[#915eff] '/>
-     <div className='w-1 sm:h-80 h-40 violet-gradient '/> 
-     </div>
+    <section className="relative w-full mx-auto h-screen ">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[180px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff] " />
+          <div className="w-1 sm:h-80 h-40 violet-gradient " />
+        </div>
 
-     <div>
-     <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-[#915eff]'>Aakanksha</span></h1>
-     <p className={`${styles.heroSubText} mt-2 text-white-100 `}>Enthusiastic and Aspiring Web Developer
-     <br className='sm:block hidden'/> 
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I am <span className="text-[#915eff]">Aakanksha</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
+            Results-oriented Full-Stack Developer passionate about <br />{" "}
+            building Web solutions that drive user engagement <br /> and
+            business growth.
+            <br className="sm:block hidden" />
+          </p>
+          <div className="hidden sm:flex">
+          <button className="mt-12 ml-5 flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
+          <svg viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#FFFFFF" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>
+          <a href="https://github.com/itsAakanksha">Github</a>
+        </button>
+
+        <button className="mt-12 ml-5 flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
+        <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 512 512"
+      stroke-width="0"
+      fill="currentColor"
+      stroke="currentColor"
      
-     </p>
-     </div>
-     </div> 
-{
-    //  <ComputersCanvas/>  
-}
-     <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
-     <a href="#about">
-     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-     <motion.div
-     animate ={{y:[0,24,0]}}
-     transition ={{
-      duration:0.5,
-      repeat:Infinity,
-      repeatType:"loop"
+    >
+      <path
+        d="M444.17 32H70.28C49.85 32 32 46.7 32 66.89v374.72C32 461.91 49.85 480 70.28 480h373.78c20.54 0 35.94-18.21 35.94-38.39V66.89C480.12 46.7 464.6 32 444.17 32zm-273.3 373.43h-64.18V205.88h64.18zM141 175.54h-.46c-20.54 0-33.84-15.29-33.84-34.43 0-19.49 13.65-34.42 34.65-34.42s33.85 14.82 34.31 34.42c-.01 19.14-13.31 34.43-34.66 34.43zm264.43 229.89h-64.18V296.32c0-26.14-9.34-44-32.56-44-17.74 0-28.24 12-32.91 23.69-1.75 4.2-2.22 9.92-2.22 15.76v113.66h-64.18V205.88h64.18v27.77c9.34-13.3 23.93-32.44 57.88-32.44 42.13 0 74 27.77 74 87.64z"
+      ></path>
+    </svg>
+      <a href="https://www.linkedin.com/in/aakankshakumarii/">Linkedin</a>
+      </button>
+      </div>
+        </div>
+        
+      </div>
+      {
+        //  <ComputersCanvas/>
+      }
 
-     }}
-     className ="w-3 h-3 rounded-full bg-secondary mb-1"
-     />
-     </div>
-     </a>
-     </div>
+     
+     
+ 
+
+      <div className="absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              dd
+              transition={{
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
